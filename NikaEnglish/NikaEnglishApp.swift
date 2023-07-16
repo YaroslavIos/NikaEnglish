99072050dc7ab9e031e21de6168656016822c027
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-07-16 16:53:04 +0300

    Initial Commit

---
+++ NikaEnglish/NikaEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikaEnglishApp.swift
+//  NikaEnglish
+//
+//  Created by Ярослав Любиченко on 16.7.2023.
+//
+
+import SwiftUI
+
+@main
+struct NikaEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
