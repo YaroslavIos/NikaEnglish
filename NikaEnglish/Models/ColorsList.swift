080ea57a2a499c16369c7b6e416e41cb6db4ff50
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-08-15 23:15:26 +0300

    Create Models.  Added files with structures.

---
+++ NikaEnglish/Models/ColorsList.swift
@@ -0,0 +1,15 @@
+//
+//  ColorsList.swift
+//  NikaEnglish
+//
+//  Created by Ярослав Любиченко on 16.7.2023.
+//
+
+import Foundation
+
+struct ColorsList: Identifiable, Codable {
+    let id: String
+    let name: String
+    let englishName: String
+    let color: String
+}
---
+++ NikaEnglish/Models/Numbers.swift
@@ -0,0 +1,15 @@
+//
+//  Numbers.swift
+//  NikaEnglish
+//
+//  Created by Ярослав Любиченко on 7.8.2023.
+//
+
+import Foundation
+
+struct Numbers: Identifiable, Codable {
+    let id: String
+    let number: String
+    let pronunciationOfTheNumber: String
+    let russianEntryOfTheNumber: String
+}
