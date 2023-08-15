ce16dafe2f9b6fd0d90472688d9e414d7fda26ca
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-08-15 23:02:42 +0300

    Create extension for working with json file. We use it to decode json file.

---
+++ NikaEnglish/CodableExtension.swift
@@ -0,0 +1,28 @@
+//
+//  CodableExtension.swift
+//  NikaEnglish
+//
+//  Created by Ярослав Любиченко on 3.8.2023.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to locate \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
