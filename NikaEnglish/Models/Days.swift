6cd4aaa795e13cfe006befb0ec9284ea09152954
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-09-17 21:17:37 +0300

    Added a new Model to app.

---
+++ NikaEnglish/Models/Days.swift
@@ -0,0 +1,15 @@
+//
+//  Days.swift
+//  NikaEnglish
+//
+//  Created by Ярослав Любиченко on 16.9.2023.
+//
+
+import Foundation
+
+struct Days: Identifiable, Codable {
+    let id: String
+    let day: String
+    let pronunciationEnglish: String
+    let pronunciationRussian: String
+}
